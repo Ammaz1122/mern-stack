91105952927226b91fc0a92b6792379f7dd126d2
Ammaz Ahmed <mmzhmd@gmail.com>
2023-04-13 11:16:21 +0500

    calculator code

---
+++ js/calculator.js
@@ -0,0 +1,94 @@
+// let a = ()=>{
+//     console.log(" This is an arrow function ");
+// }
+// a();
+
+// function sum(a,b){
+//     let sum = a + b;
+//     return sum;
+// }
+
+// let a = sum(10,20);
+// console.log(" Sum is = " + a);
+
+// let add = ()=>{
+//     console.log(" This is anonymous Function ");
+// }
+// add();
+
+
+// let a = prompt(" Enter a first number ");
+// let b = prompt("Enter as second number ");
+let x = parseInt(prompt("Enter a firs number "));
+let y = parseInt(prompt("Enter a second number "));
+let choice =parseInt(prompt("Enter a choice  "));
+
+
+    if(choice == 1)
+    {
+        let sum = parseInt(x) + parseInt(y);
+        alert(`Sum is = ${sum}`);
+        // console.log("Sum is = ",parseInt(sum),);
+    }
+
+   else if(choice == 2)
+    {
+        let sub = parseInt(x) - parseInt(y);
+        alert(`sub is = ${sub}`);
+    }
+
+    
+   else if(choice == 3)
+   {
+       let mult = parseInt(x) * parseInt(y);
+       alert(`Multiply is = ${mult}`);
+   }
+
+   else if(choice == 4)
+   {
+       let Divide = parseInt(x) / parseInt(y);
+       alert(`Divide is = ${Divide}`);
+   }
+    
+
+
+
+
+
+// let add = (a,b)=>{
+//     return a + b;
+// }
+// let a  =add(10,20);
+// console.log("Sum is = ",a);
+
+// let sub = (a,b)=>{
+//     return a - b;
+// }
+// let s = sub(50,10);
+// console.log(s);
+
+// let mult = (a,b)=>{
+//     return a * b;
+// }
+// let m = sub(5,11);
+// console.log(m);
+
+
+// let Divide = (a , b)=>{
+// return a / b;
+
+// }
+// let d = Divide(10,2);
+// console.log(" Divide is = ",d);
+
+
+// let mod = (a,b)=>{
+//     return a % b;
+// }
+
+// let modulus = mod(13,5);
+// console.log(" Modulus is = ",modulus)
+
+
+// let choice = prompt(" Enter your choice form user ");
+
