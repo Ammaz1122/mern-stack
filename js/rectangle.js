a3c2326ad197dedc234fc5c49d5df141f5b0b30c
Ammaz Ahmed <mmzhmd@gmail.com>
2023-04-13 11:15:17 +0500

    show the width and height of rectangle

---
+++ js/rectangle.js
@@ -0,0 +1,10 @@
+
+
+let rectangle = (width,height)=>{
+   return width * height;
+   
+}
+ let width = prompt("Enter a value of width ");
+ let height = prompt(" Enter the height of Rectangle ");
+ let rec = rectangle(width,height);
+ alert(`Rectagle of ${width} and ${height} is: ${rec}`)
